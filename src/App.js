17b1bffd767a030ff7e9d2b736878104ee501233
Ashik <nafisfoisal25@gmail.com>
2026-01-13 11:19:04 +0600

    register functionality added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Room from "./pages/Room";
+import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/room/:roomId" element={<Room />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
